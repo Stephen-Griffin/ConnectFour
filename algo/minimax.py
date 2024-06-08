25bff6a13cad835a384f4f9eab0019296aedeb0e
Stephen Griffin <stephengriffin2210@gmail.com>
2024-06-08 01:08:29 -0400

    Finished terminal state function

---
+++ algo/minimax.py
@@ -0,0 +1,4 @@
+from ConnectFour.algo.terminal import is_terminal
+
+# alpha-beta pruning algorithm 
+def abp_minimax(board, depth, alpha, beta, max): NotImplemented
--- algo/terminal.py
+++ algo/terminal.py
@@ -1,4 +1,6 @@
-# Function returns true if board in terminal position (i.e next placement = win)
+# Functional class that defines whether or not the board position is in the END state.
+# This is determined by a win, or tie if the board is filled
+
 def is_terminal(board):
     # checking vertical win possibility 
     for col in range(7):
@@ -12,13 +14,20 @@ def is_terminal(board):
             if board[row][col] == board[row][col+1] == board[row][col+2] == board[row][col+3] != 0:
                 return True
             
-    # checking sloped (up and to the right)
-    for col in range (4):
+    # checking sloped up 
+    for col in range(4):
         for row in range(3):
-            if board[row][col] == board[row][col]
-
-
-
-
-# alpha-beta pruning algorithm 
-def abp_minimax(board, depth, alpha, beta, max): NotImplemented
+            if board[row][col] == board[row+1][col+1] == board[row+2][col+2] == board[row+3][col+3] != 0:
+                return True
+            
+    # checking sloped down 
+    for col in range(4):
+        for row in range(3, 6): 
+            if board[row][col] == board[row-1][col+1] == board[row-2][col+2] == board[row-3][col+3] != 0:
+                return True
+            
+    # if board is full, return false
+    if any(0 in row for row in board):
+        return False
+    
+    return False
