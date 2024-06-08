c7d29ed204bc86ac0f496f90b9372626333e19d1
Stephen Griffin <stephengriffin2210@gmail.com>
2024-06-08 00:25:14 -0400

    Added initial version of algorithm (in progress)

---
+++ algo/Algorithm.py
@@ -0,0 +1,22 @@
+# Function returns true if board in terminal position (i.e next placement = win)
+def is_terminal(board):
+    # checking vertical win possibility 
+    for col in range(7):
+        for row in range(3):
+            if board[row][col] == board[row+1][col] == board[row+2][col] == board[row+3][col] != 0:
+                return True
+            
+    # checking horizontal win possibility
+    for col in range(4):
+        for row in range(6):
+            if board[row][col] == board[row][col+1] == board[row][col+2] == board[row][col+3] != 0:
+                return True
+            
+    # checking sloped (up and to the right)
+
+
+
+
+
+# alpha-beta pruning algorithm 
+def abp_minimax(board, depth, alpha, beta, max): NotImplemented
