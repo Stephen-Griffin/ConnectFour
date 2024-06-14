d1f552ce538fc17f3854f1af8d9867f1a50cea78
Stephen Griffin <stephengriffin2210@gmail.com>
2024-06-13 20:26:30 -0400

    placing objects at bottom of ui

--- algo/main.py
+++ algo/main.py
@@ -17,6 +17,7 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
 
 pygame.init()
 screen = pygame.display.set_mode(size)
@@ -30,14 +31,14 @@ def draw_board(board):
     for c in range(COLUMN_COUNT):
         for r in range(ROW_COUNT):
             pygame.draw.rect(screen, BLUE, (c * SQUARESIZE, r * SQUARESIZE + SQUARESIZE, SQUARESIZE, SQUARESIZE))
-            pygame.draw.circle(screen, BLACK, (int(c * SQUARESIZE + SQUARESIZE / 2), int(r * SQUARESIZE + SQUARESIZE + SQUARESIZE / 2)), RADIUS)
+            pygame.draw.circle(screen, WHITE, (int(c * SQUARESIZE + SQUARESIZE / 2), int(r * SQUARESIZE + SQUARESIZE + SQUARESIZE / 2)), RADIUS)
     
     for c in range(COLUMN_COUNT):
         for r in range(ROW_COUNT):        
             if board[r][c] == 'R':
-                pygame.draw.circle(screen, RED, (int(c * SQUARESIZE + SQUARESIZE / 2), int((ROW_COUNT - r - 1) * SQUARESIZE + SQUARESIZE + SQUARESIZE / 2)), RADIUS)
+                pygame.draw.circle(screen, RED, (int(c * SQUARESIZE + SQUARESIZE / 2), height - int((r+1) * SQUARESIZE - SQUARESIZE / 2)), RADIUS)
             elif board[r][c] == 'Y': 
-                pygame.draw.circle(screen, YELLOW, (int(c * SQUARESIZE + SQUARESIZE / 2), int((ROW_COUNT - r - 1) * SQUARESIZE + SQUARESIZE + SQUARESIZE / 2)), RADIUS)
+                pygame.draw.circle(screen, YELLOW, (int(c * SQUARESIZE + SQUARESIZE / 2), height - int((r+1) * SQUARESIZE - SQUARESIZE / 2)), RADIUS)
     pygame.display.update()
 
 def main():
